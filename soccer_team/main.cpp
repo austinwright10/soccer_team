77e1e0d4f18c4de18445294b24b9b86b23199690
austinwright10 <austwright10@gmail.com>
2024-02-02 20:35:26 -0700

    Initial Commit

---
+++ soccer_team/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  soccer_team
+//
+//  Created by Austin Wright on 2/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
